00f5f50d144a19584f311c5cdf057733f6bee640
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-11-03 00:11:44 +0530

    Updated MessagesMongoRepository

--- src/main/java/com/secme/repository/MessagesMongoRepository.java
+++ src/main/java/com/secme/repository/MessagesMongoRepository.java
@@ -2,9 +2,11 @@ package com.secme.repository;
 
 import com.secme.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MessagesMongoRepository extends MongoRepository<Message, String> {
 
     //find messages by the users who posted them
