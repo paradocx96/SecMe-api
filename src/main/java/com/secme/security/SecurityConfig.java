61a70e8c95eb679db944e54ad1b6b8b5ba56bc37
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-02 21:24:30 +0530

    Scope Test

--- src/main/java/com/secme/security/SecurityConfig.java
+++ src/main/java/com/secme/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .mvcMatchers("/api/post/private").authenticated()
                 .mvcMatchers("/api/post/scopeadmin").hasAuthority("read_admin:messages")
                 .mvcMatchers("/api/post/scopemanager").hasAuthority("read:messages")
-                .mvcMatchers("/api/post/scopeadmin").hasAuthority("SCOPE_read:messages")
+                .mvcMatchers("/api/post/scopeworker").hasAuthority("SCOPE_read:messages")
                 .and().cors()
                 .and().oauth2ResourceServer().jwt();
         return http.build();
