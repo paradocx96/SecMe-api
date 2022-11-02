9d66c060ea37ef8b9b50d2087248c98e80ef052a
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-11-03 01:38:15 +0530

    Added MessagesApi

---
+++ src/main/java/com/secme/api/MessagesApi.java
@@ -0,0 +1,63 @@
+package com.secme.api;
+
+/*
+ * IT19014128 - A.M.W.W.R.L. Wataketiya
+ *
+ * The API class for Messages
+ * Performs processing of data from controllers
+ * */
+
+import com.secme.adapter.MessagesAdapter;
+import com.secme.converters.MessageDtoConverter;
+import com.secme.dto.MessageDto;
+import com.secme.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MessagesApi {
+
+    private final MessagesAdapter messagesAdapter;
+
+    @Autowired
+    public MessagesApi(MessagesAdapter messagesAdapter) {
+        this.messagesAdapter = messagesAdapter;
+    }
+
+    //method to create the message
+    public MessageDto createMessage(MessageDto messageDto){
+        Message message = MessageDtoConverter.convertDtoToModelWithNoId(messageDto);
+        Message createdMessage = messagesAdapter.save(message);
+        return MessageDtoConverter.convertModelToDtoWithId(createdMessage);
+    }
+
+    //get all the messages in the system
+    public List<MessageDto> getAlMessages(){
+        List<Message> messages = messagesAdapter.getAll();
+        List<MessageDto> messageDtos  = new ArrayList<>();
+        for (Message message :  messages){
+            MessageDto messageDto = MessageDtoConverter.convertModelToDtoWithId(message);
+            messageDtos.add(messageDto);
+        }
+        return messageDtos;
+    }
+
+    //get all messages by creator's username
+    public List<MessageDto> getMessagesByUsername(String username){
+        List<Message> messages = messagesAdapter.getByUsername(username);
+        List<MessageDto> messageDtos  = new ArrayList<>();
+        for (Message message :  messages){
+            MessageDto messageDto = MessageDtoConverter.convertModelToDtoWithId(message);
+            messageDtos.add(messageDto);
+        }
+        return messageDtos;
+    }
+
+    //delete a message by id
+    public String deleteMessage(String id){
+        return messagesAdapter.delete(id);
+    }
+}
