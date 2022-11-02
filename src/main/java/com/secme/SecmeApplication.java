109e9902c187845147229d713ae4f34dfcc50019
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-02 16:13:46 +0530

    Project Init

---
+++ src/main/java/com/secme/SecmeApplication.java
@@ -0,0 +1,13 @@
+package com.secme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecmeApplication.class, args);
+	}
+
+}
