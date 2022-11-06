6670db6c785526b9962c7038d92b5cbb72f33e89
Hirush Gimhan <it19240848@my.sliit.lk>
2022-11-07 00:55:11 +0530

    Created file response dto class

---
+++ src/main/java/com/secme/dto/FileResponseDto.java
@@ -0,0 +1,30 @@
+package com.secme.dto;
+
+public class FileResponseDto {
+    String message;
+    String status;
+
+    public FileResponseDto() {
+    }
+
+    public FileResponseDto(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
