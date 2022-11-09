18562c301620f5f6294bd6af4be07bb92c1c7aba
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-09 22:17:58 +0530

    Files Updated

--- src/main/java/com/secme/controller/AuthController.java
+++ src/main/java/com/secme/controller/AuthController.java
@@ -1,40 +1,40 @@
 package com.secme.controller;
 
-import com.secme.model.Post;
+import com.secme.model.Auth;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/post")
-public class PostController {
+@RequestMapping(path = "api/auth")
+public class AuthController {
 
     @GetMapping(value = "/public")
-    public Post publicEndpoint() {
-        return new Post("All good. You DO NOT need to be authenticated to call /api/public.");
+    public Auth publicEndpoint() {
+        return new Auth("All good. You DO NOT need to be authenticated to call /api/public.");
     }
 
     @GetMapping(value = "/private")
-    public Post privateEndpoint() {
-        return new Post("All good. You can see this because you are Authenticated.");
+    public Auth privateEndpoint() {
+        return new Auth("All good. You are Authenticated.");
     }
 
     @GetMapping(value = "/scopeadmin")
     @PreAuthorize("hasAuthority('read:admin-post')")
-    public Post privateScopedAdminEndpoint() {
-        return new Post("All good. Role:Amin");
+    public Auth privateScopedAdminEndpoint() {
+        return new Auth("All good. Role:Amin");
     }
 
     @GetMapping(value = "/scopemanager")
     @PreAuthorize("hasAuthority('read:manager-post')")
-    public Post privateScopedManagerEndpoint() {
-        return new Post("All good. Role:Manager");
+    public Auth privateScopedManagerEndpoint() {
+        return new Auth("All good. Role:Manager");
     }
 
     @GetMapping(value = "/scopeworker")
     @PreAuthorize("hasAuthority('read:worker-post')")
-    public Post privateScopedWorkerEndpoint() {
-        return new Post("All good. Role:Worker");
+    public Auth privateScopedWorkerEndpoint() {
+        return new Auth("All good. Role:Worker");
     }
 }
