faa97220e11837235204556fde8ed343b1812d41
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-16 18:14:33 +0530

    Security Update

--- src/main/java/com/secme/security/SecurityConfig.java
+++ src/main/java/com/secme/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .mvcMatchers("/api/auth/public").permitAll()
                 .antMatchers(
                         "/api/**",
                         "/api/auth/**",
